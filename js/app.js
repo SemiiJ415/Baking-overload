ad383d8f353423ef185cd0721cb8bd756ddfc175
SemiiJ415 <Jarissa415@gmail.com>
2024-02-15 15:01:19 -0600

    fixes

--- js/app.js
+++ js/app.js
@@ -19,9 +19,9 @@ howToPlayBtn = document.getElementById('howToPlayBtn')
 helpBtn = document.getElementById('helpBtn')
 answerChoices = document.getElementsByClassName('answer-choices')
 
-const list = document.getElementById('list')
-const listItem = document.createElement('li')
-listItem.classList.add('ingredient-list-item', 'text-capitalize', 'list-unstyled')
+// const list = document.getElementById('list')
+// const listItem = document.createElement('li')
+// listItem.classList.add('ingredient-list-item', 'text-capitalize', 'list-unstyled')
 
 const answerBoxes = document.querySelector('.answer-boxes')
 
@@ -95,12 +95,41 @@ var dessertItems = [
         qty: 12
     }
 ];
+// const btnClicked=()=>{
+//     brownieBtn = document.getElementById('brownieBtn')
+//     brownieBtn.addEventListener('click', ()=>{
+    
+// })}
 
+// console.log(btnClicked())
+// }
+// btnClicked()
+const connectType=(btn)=>{
+    for (let i = 0; i < dessertItems.length; i++){
+            if(btn.dataset.item === dessertItems[i].item){
+                // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration
+                let currentItem = dessertItems[i].item
+                console.log(currentItem)
+                for(const item of Object.entries(currentItem)){
+                    // var listItem = document.createElement('li')
+                    // list.appendChild(listItem)
+                    // listItem.innerText = item}
+                    if (currentItem == questionCategory){
+                        console.log('true')
+                    }
+                }
+                // console.log(questionCategory)
+            }}
+    // console.log(dessertItems)
+
+}
 //whichever dessert is selected the ingredients need to populate on the ingredients list
 const dessertBtns = document.querySelectorAll('.dessertBtns')
     dessertBtns.forEach(btn =>{
         btn.addEventListener('click', ()=>{
-            buildList()
+            buildList(btn)
+            connectType(btn)
+            displayQuestion()
         })
     })
 
@@ -108,27 +137,30 @@ const removeLI =(parent, child)=> {
     parent.removeChild(child)
 }
 
-const buildList =()=> {
-    const ingredientListItems = document.querySelectorAll('.ingredient-list-item')
 
+const buildList =(btn)=> {
+    // console.log(dessertBtns);
+    for (let i = 0; i < dessertItems.length; i++){
+        if(btn.dataset.item === dessertItems[i].item){
+            // var list = document.getElementById('list')
+            // for (let ingredient in dessertItems[i].ingredients)
+
+            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration
+            let currentItem = dessertItems[i].ingredients
+            console.log(currentItem)
+            for(const [ingredient, amt] of Object.entries(currentItem)){
+                var listItem = document.createElement('li')
+                list.appendChild(listItem)
+                listItem.innerText = `${ingredient}: ${amt}`}
+            }
+        }
+    const ingredientListItems = document.querySelectorAll('.ingredient-list-item')
     ingredientListItems.forEach(item => {
         removeLI(list, item)
     })
     
-    for (let i = 0; i < dessertItems.length; i++){
-        if(dessertBtns.dataset.item === dessertItems[i].item){
-            console.log(dessertItems[i])
-            // var list = document.getElementById('list')
-            for (const ingredient in dessertItems[i][ingredient]) {
-                // var listItem = document.createElement('li')
-                list.appendChild(listItem)
-                listItem.innerText = `${ingredient}: ${dessertItems[i][ingredient]}`}
-            }
-        }
         
     }
-
-
 
 const questions = [
         {
@@ -244,83 +276,147 @@ const questions = [
             correctChoice: '1'
         },
     ]
-const randomIndex = Math.floor(Math.random() * questions.length)
-const randomQuestion = questions[randomIndex]
-
-console.log(randomQuestion)
-
-const quizChoices = questions[0].choices
-    
-quizChoices.forEach(choice => {
-    const choiceButton = document.createElement('button')
-    choiceButton.classList.add('answer-choices')
-    choiceButton.setAttribute('id', `choice${quizChoices.indexOf(choice) + 1}`)
-    choiceButton.dataset.choice = choice
-    choiceButton.innerText = choice
-    
-    answerBoxes.appendChild(choiceButton)
-})
-// brownies button selected. only lists ingredients and displays first random question.
-brownies = document.getElementById('brownieBtn')
-brownies.addEventListener('click', ()=>{
-    const brownieIngredients = dessertItems[0].ingredients
-    // console.log(brownieIngredients)
-
-    var list = document.getElementById('list')
-    for (const ingredient in brownieIngredients) {
-        var listItem = document.createElement('li')
-        listItem.classList.add('ingredient-list-item', 'text-capitalize', 'list-unstyled')
-        list.appendChild(listItem)
-        listItem.innerText = `${ingredient}: ${brownieIngredients[ingredient]}`
-
-        // browniesCicked = false
-        // if (!browniesCicked) {
-        //     brownies.classList.add('disable')
-        // }
-        console.log(`${ingredient}: ${brownieIngredients[ingredient]}`)
-
-        // brownie questions. 
-        var brownieQuestions = [
-            'To make a pan of 16 brownies you need 1 cup of brown sugar. Tommy wants 48 brownies, how many cups of brown sugar do you need?', 
-            'To make a pan of 16 brownies you need 4 eggs. Jennifer wants 8 brownies, how many eggs do you need?',
-            'To make a pan of 16 brownies you need 1 tablespoon of vanilla extract. Mark wants 32 brownies, how many tablespoons of vanilla extract do you need?',
-            'To make a pan of 16 brownies you need 1 cup of all purpose flour. Jamiya wants 16 brownies, how many ounces of flour do you need?'
-                // {
-                    // id: 1,
-                    // brnQuestion: 'To make a pan of 16 brownies you need 1 cup of brown sugar. Tommy wants 48 brownies, how many cups of brown sugar do you need?',
-                    // solution: 3,
-                // },
-                // {
-                //     id: 2,
-                //     brnQuestion: 'To make a pan of 16 brownies you need 4 eggs. Jennifer wants 8 brownies, how many eggs do you need?',
-                //     solution: 2,
-                // },
-                // {
-                //     id: 3,
-                //     question: 'To make a pan of 16 brownies you need 1 tablespoon of vanilla extract. Mark wants 32 brownies, how much vanilla extract do you need?',
-                //     solution: 2,
-                // },
-                // {
-                //     id: 4,
-                //     question: 'To make a pan of 16 brownies you need 1 cup of all purpose flour. Jamiya wants 16 brownies, how many ounces of flour do you need?',
-                //     solution: 2,
-                // }
-        ]
-        // brownie answers
-        var brownieAnswers = [3, 2, 2, 2]
-        // grab random brownie question
-        var randomIndex = Math.floor(Math.random() * brownieQuestions.length)
-        var randomBrownieQuestion = brownieQuestions[randomIndex]
-        
-            // display random brownie question
-            question = document.getElementById('question')
-            question.innerText = `${randomBrownieQuestion}`
-
-            // var randomAnswerIndex = Math.floor(Math.random() * brownieAnswers.length)
-            // var randomBrownieAnswer = brownieAnswers[randomIndex]
-            // choice1.innerText = randomBrownieAnswer
+const questionIds = []
+    for (let i = 0; i < questions.length; i++){
+        questionIds.push(questions[i].id)
     }
+    console.log(questionIds)
+    // questionIds.sort(() => Math.random() - 0.5)
+const questionCategory = []
+    for(let i = 0; i < questions.length; i++) {
+        questionCategory.push(questions[i].category)
+    }
+    // console.log(questionCategory)
+/**
+ * Filter array items based on search criteria (query)
+ */
+function filterItems(arr, query) {
+    return arr.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
+}
+brnCategory = (filterItems(questionCategory, "brown")); 
+cookCategory = (filterItems(questionCategory, "cook"));
+cupCategory = (filterItems(questionCategory, "cup"));
+panCategory = (filterItems(questionCategory, "pan"));
+
+let innerQuestion = []
+    for (let i = 0; i < questions.length; i++){
+        innerQuestion.push(questions[i].question)   
+    }
+    console.log(innerQuestion)
+const questionChoices = []
+    for (let i = 0; i < questions.length; i++){
+        questionChoices.push(questions[i].choices) 
+    }
+    console.log(questionChoices)
+const correctAnswer = []
+    for (let i = 0; i < questions.length; i++){
+        correctAnswer.push(questions[i].correctChoice)
+    }
+    console.log(correctAnswer)
+
+const displayQuestion=()=>{
+    // console.log(questionIds, innerQuestion);
+    let index = 0
+        if(questionIds.indexOf == innerQuestion.indexOf) {
+            const question = document.getElementById('question')
+            question.innerText = innerQuestion[index]
+            displayAnswerChoices()
+        }
+    }
+const displayAnswerChoices=()=>{
+    let questChoices = questionChoices[0]
+    console.log(questChoices)
+    for (let i = 0; i < questChoices.length; i++){
+
+        const choice1 = document.getElementById('choice1')
+        const choice2 = document.getElementById('choice2')
+        const choice3 = document.getElementById('choice3')
+        
+
+            // btn = document.createElement('button')
+            // btn.classList.add('answer-choices')
+            // btn.innerText = questChoices
+            // answerBoxes.appendChild(btn)
+        }
+
+    }
+    // if(questionIds.indexOf == innerQuestion.indexOf) {
+    //     const choiceBtn = document.createElement('button')
+    //     questChoices.forEach(btn => {
+    //         choiceBtn.classList.add('answer-choices')
+    //         // choiceBtn.setAttribute('id', `choice${answerKey.indexOf(choice) + 1}`)
+    //         choiceBtn.dataset.choice = btn
+    //         choiceBtn.innerText = questChoices
+                
+    //             answerBoxes.appendChild(choiceBtn)
+            
+    //     });
+        // console.log(choiceBtn)
+        // }
+// }
+
+const checkAnswer=(innerQuestion, questionChoices, correctAnswer)=>{
+    getAnswerKey()
+
+}
+
+const nextQuestion=()=>{
+    const next = document.getElementById('nextBtn')
+    
+    next.addEventListener('click', ()=>{
+        const question = document.getElementById('question')
+        question.innerText = innerQuestion[1]
+
     })
+// console.log(innerQuestion[1])
+
+}
+nextQuestion()
+    // displayQuestion()
+    
+
+    // const categoryResults = questionCategory.filter(brownie)
+    // if (questionCategory == dessertItems.item){
+    //     console.log('true')
+    // }
+
+
+    // for (let i = 0; i < questionIds.length; i++) {
+    //     questions.forEach((item) => {
+    //         if(item.id == questionIds[i]) {
+    //             const question = document.getElementById('question')
+    //             question.innerText = (item.question)
+    //             // console.log(item.question)
+    //         }
+    //     })
+    // }
+
+
+// const randomIndex = Math.floor(Math.random() * questions.length)
+// const randomQuestion = questions[randomIndex]
+
+// console.log(randomQuestion)
+
+const answerKey = questionChoices
+    // console.log(answerKey)
+const getAnswerKey=()=> {
+        answerKey.forEach(choice => {
+            const choiceButton = document.createElement('button')
+            choiceButton.classList.add('answer-choices')
+            choiceButton.setAttribute('id', `choice${answerKey.indexOf(choice) + 1}`)
+            choiceButton.dataset.choice = choice
+            choiceButton.innerText = choice
+            
+            answerBoxes.appendChild(choiceButton)
+        })
+    }
+    // getAnswerKey()
+
+
+
+// console.log(correctAnswer)
+// brownies button selected. only lists ingredients and displays first random question.
+//   
 
 
     // trying to get answer choices to display
@@ -340,7 +436,7 @@ chocChip.addEventListener('click', ()=>{
             listItem.classList.add('ingredient-list-item', 'text-capitalize', 'list-unstyled')
             list.appendChild(listItem)
             listItem.innerText = `${ingredient}: ${chocChipIngredients[ingredient]}`
-            console.log(`${ingredient}: ${chocChipIngredients[ingredient]}`)
+            // console.log(`${ingredient}: ${chocChipIngredients[ingredient]}`)
             // choc chip questions
             var chocChipCookieQuestions = [
                 'For every 12 chocolate chip cookies you need a whole egg. Morty wants 3 dozen chocolate chip cookies. How many eggs do you need?',
@@ -392,7 +488,7 @@ cupcake.addEventListener('click', ()=>{
             
             list.appendChild(listItem)
             listItem.innerText = `${ingredient}: ${cupcakeIngredients[ingredient]}`
-            console.log(`${ingredient}: ${cupcakeIngredients[ingredient]}`)
+            // console.log(`${ingredient}: ${cupcakeIngredients[ingredient]}`)
             // cupcake questions
             var cupcakesQuestions = [
                 'For every 24 cupcakes you need 4 whole eggs. Keisha wants 3 dozen cupcakes, how many eggs do you need?',
@@ -443,7 +539,7 @@ pancake.addEventListener('click', ()=>{
             
             list.appendChild(listItem)
             listItem.innerText = `${ingredient}: ${pancakeIngredients[ingredient]}`
-            console.log(`${ingredient}: ${pancakeIngredients[ingredient]}`)
+            // console.log(`${ingredient}: ${pancakeIngredients[ingredient]}`)
             var pancakesQuestions = [
                 'For every 12 pancakes you need 186.5 grams of all purpose flour. Dhon wants a dozen pancakes, how many cups of all purpose flour do you need?',
                 'For every 12 pancakes you need 1 cup of milk. Maggie wants a 4 dozen pancakes, how many cups of milk do you need?',
@@ -479,7 +575,7 @@ pancake.addEventListener('click', ()=>{
                 question = document.getElementById('question')
                 question.innerText = `${randomPancakesQuestion}`
                 
-                console.log(pancakesQuestions)
+                // console.log(pancakesQuestions)
         }
         })
 
